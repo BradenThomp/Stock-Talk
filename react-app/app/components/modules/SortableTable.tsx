c2858b9617566a7d5124f136231161a27d3a0c54
Braden <bradent@outlook.com>
2021-05-06 21:02:08 -0600

    added sorting to sortable table

--- react-app/app/components/modules/SortableTable.tsx
+++ react-app/app/components/modules/SortableTable.tsx
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { TablePagination } from "@material-ui/core";
+import { TablePagination, TableSortLabel } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -28,11 +28,45 @@ export interface Column{
   label: any;
 }
 
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
 export default function SortableTable(props: Props){
   const classes = useStyles();
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+  const [orderBy, setOrderBy] = useState<string>(null);
+
+  const createSortHandler = (property) => (event) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  }
+
   const handleChangePage = (event: any, newPage: number) => {
     setPage(newPage)
   }
@@ -49,14 +83,16 @@ export default function SortableTable(props: Props){
           <TableHead>
             <TableRow>
               {props.columns.map((column: Column) => (
-                <TableCell key={column.key}>
-                  {column.label}
+                <TableCell key={column.key} sortDirection={orderBy === column.key ? order : false}>
+                  <TableSortLabel active={orderBy === column.key} direction={orderBy === column.key ? order : 'asc'} onClick={createSortHandler(column.key)}>
+                    {column.label}
+                  </TableSortLabel>
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row: any) => {
+            {stableSort(props.rows, getComparator(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row: any) => {
               return (
                 <TableRow hover>
                   {props.columns.map((column: Column) => {
@@ -65,10 +101,10 @@ export default function SortableTable(props: Props){
                       <TableCell key={column.key}>
                         {value}
                       </TableCell>
-                    )
+                    );
                   })}
                 </TableRow>
-              )
+              );
             })}
           </TableBody>
         </Table>
